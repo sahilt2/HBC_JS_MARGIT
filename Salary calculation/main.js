30050f5ecb7460059ef76eac377aaedeeb4a4b7c
Thapa Sahil <s2300123@BC-C02GQ5FAQ6LR-MacBook-Air.local>
2023-01-23 09:46:48 +0200

    salary calculation

---
+++ Salary calculation/main.js
@@ -0,0 +1,15 @@
+function salaryCal() {
+    const hourlySalary = prompt("Enter your hourly salary")
+    const hoursWorked = prompt ("Enter your working hours in a day")
+    if (hoursWorked<=7) {
+      console.log(`Your salary is ${hourlySalary*hoursWorked} euros in a day`);
+    }
+    else if (hoursWorked>7 && hoursWorked<9){
+        console.log(`Your salary is ${hourlySalary*hoursWorked+((hoursWorked-7)*(hourlySalary*0.5) )} euros in a day`);
+    }
+    else {
+        console.log(`Your salary is ${hourlySalary*hoursWorked+((hourlySalary*0.5*2))+((hoursWorked-9)*hourlySalary )} euros in a day`)
+    }
+    }
+
+salaryCal()
