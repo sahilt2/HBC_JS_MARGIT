e5f9a7450a207ba2928fd7c9e6456b916fb05907
Thapa Sahil <s2300123@BC-C02GQ5FAQ6LR-MacBook-Air.local>
2023-01-23 12:41:10 +0200

    comment added

--- task_RandomNumbers/main.js
+++ task_RandomNumbers/main.js
@@ -16,3 +16,28 @@ function randomNumber(){
    
 }
 randomNumber();
+
+//alternative method
+
+/* //function randomNumber(){
+    const numOne= Number (prompt('Enter first number'));
+    const numTwo= Number (prompt('Enter second number'));
+    const numThree= Number (prompt('Enter third number'));
+    
+    const sum= numOne+numTwo+numThree;
+    const mult= numOne*numTwo*numThree;
+
+    if (numOne>=0 || numTwo>=0 || numThree>=0){
+        console.log(`The sum of input numbers is ${sum}`)
+
+    }
+    if (numOne>=0 && numTwo>=0 & numThree>=0) {
+        console.log(`The multiplication of input numbers is ${mult}`)
+    }
+    else if (numOne<0 && numTwo<0 && numThree<0){
+        console.log('only negatives')
+    }
+}
+   
+
+randomNumber(); */
